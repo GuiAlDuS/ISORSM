ab4fd7667820cae72519ebe1c6f3eb733c62d429
Guillermo A. Durán Sanabria <gds506@gmail.com>
2018-06-11 09:27:15 -0600

    Modificaciones para primer prueba
    Actualización de comandos para prueba #1

--- script_general_beta.sh
+++ script_general_beta.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 #crear nuevo directorio
-mkdir ~/Guillermo/DatosAnaMaria/$1
+mkdir ~/Guillermo/DatosAnaMaria/nc${1}
 echo 'Directorio ~/Guillermo/DatosAnaMaria/${1} creado'
 
 #copiar archivo .ctl a nuevo directorio
-rsync -a r_pgb.ctl ~/Guillermo/DatosAnaMaria/$1
+rsync -zvh r_pgb.ctl ~/Guillermo/DatosAnaMaria/nc${1}
 echo 'Archivo ctl copiado al nuevo directorio'
 
 #copiar archivo .gs al nuevo directorio
-rsync -a script_test.gs ~/Guillermo/DatosAnaMaria/$1
+rsync -zvh script_grads.gs ~/Guillermo/DatosAnaMaria/nc${1}
 echo 'Archivo script gs copiado al nuevo directorio'
 
 #ir al nuevo directorio
-cd ~/Guillermo/DatosAnaMaria/$1
+cd ~/Guillermo/DatosAnaMaria/nc${1}
 
 #editar archivo .ctl y modificar año. VERIFICAR QUE SEA SOLO EN LAS PRIMERAS DOS LINEAS
 sed -i -e '1s/1980/$1/g; 2s/1980/$1/g'  /r_pgb.ctl
 echo 'Archivo ctl actualizado a valores del año ${1}'
 
 #modificar script de grads y cambiar año por últimos dos números
-sed -i -e 's/80/${1:2:2}/g' /script_test.gs
+sed -i -e 's/80/${1:2:2}/g' /script_grads.gs
 echo 'Archivo script gs actualizado a valores del año ${1:2:2}'
 
 #correr grads con script desde la línea de comandos
-grads -bqpx "run ....."
+grads -bpcx "run script_grads.gs"
 echo 'Procesamiento de GrADS finalizado'
 
 #https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
@@ -42,5 +42,6 @@ done
 echo 'Procesamiento de CDO finalizado'
 
 #eliminar archivos intermedios
-remove !(*_d.nc)
+#shopt -s extglob
+#rm !(*_d.nc)
 echo 'Eliminación de archivos temporales lista'
