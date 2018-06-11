ee4efa0f19ca6dc282a571f44b37bee4a9d48a07
Guillermo Duran <gds506@gmail.com>
2018-06-10 20:09:28 -0600

    Script inicial. Falta arreglar lo de los comandos.

---
+++ script_general_beta.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#crear nuevo directorio
+mkdir ~/Guillermo/DatosAnaMaria/$1
+echo 'Directorio ~/Guillermo/DatosAnaMaria/${1} creado'
+
+#copiar archivo .ctl a nuevo directorio
+rsync -a r_pgb.ctl ~/Guillermo/DatosAnaMaria/$1
+echo 'Archivo ctl copiado al nuevo directorio'
+
+#copiar archivo .gs al nuevo directorio
+rsync -a script_test.gs ~/Guillermo/DatosAnaMaria/$1
+echo 'Archivo script gs copiado al nuevo directorio'
+
+#ir al nuevo directorio
+cd ~/Guillermo/DatosAnaMaria/$1
+
+#editar archivo .ctl y modificar año. VERIFICAR QUE SEA SOLO EN LAS PRIMERAS DOS LINEAS
+sed -i -e '1s/1980/$1/g; 2s/1980/$1/g'  /r_pgb.ctl
+echo 'Archivo ctl actualizado a valores del año ${1}'
+
+#modificar script de grads y cambiar año por últimos dos números
+sed -i -e 's/80/${1:2:2}/g' /script_test.gs
+echo 'Archivo script gs actualizado a valores del año ${1:2:2}'
+
+#correr grads con script desde la línea de comandos
+grads -bqpx "run ....."
+echo 'Procesamiento de GrADS finalizado'
+
+#https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
+
+#script para CDO y sumas/promedios diarios
+declare -a sumas=("CPRAT1sfc_${1:2:2}.nc" "CPRAT2sfc_${1:2:2}.nc" "CPRATsfc_${1:2:2}.nc")
+
+for files in ./*.nc; do
+	if [[ "${sumas[@]}" =~ "$files" ]]; then
+		cdo daysum "$files" "${files%.*nc}_d.nc"
+	else
+		cdo daymean "$files" "${files%.*nc}_d.nc"
+	fi 
+done
+echo 'Procesamiento de CDO finalizado'
+
+#eliminar archivos intermedios
+remove !(*_d.nc)
+echo 'Eliminación de archivos temporales lista'
